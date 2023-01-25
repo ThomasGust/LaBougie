8a8fc6a6b88edf2c2231286a23281f6811b96be3
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2023-01-24 21:22:51 -0800

    Add test box
    Create a box that shrinks once timer has elapsed

--- labougie.py
+++ labougie.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import sys
+from modules import Candle
 
 pygame.init()
 
@@ -12,7 +13,8 @@ def run():
     screen = pygame.display.set_mode((X, Y))
     font = pygame.font.SysFont('monospace', 15)
 
-    stop = 1.0*60.0*60.0
+    candle = Candle("test_candle", 1.0*60.0, (255, 0, 0))
+
     pygame.display.set_caption('LaBougie')
     st = time.time()
     running = True
@@ -22,20 +24,25 @@ def run():
             if event.type == pygame.QUIT:
                 running = False
                 sys.exit()
-
             
         screen.fill((255, 255, 255))
 
         s = time.time() - st
+        candle.e_time = s
 
-        if s >= stop:
+        if s >= candle.burn_time:
             running = False
             sys.exit()
-        text = f"{(s/stop)*100}%"
+        
+        percentage = s/candle.burn_time
+        text = f"{percentage*100:.2f}%"
+
         tl = font.render(text, 1, (0,0,0))
         screen.blit(tl, (X-200, Y-200))
 
-        pygame.display.update()
+        rectangle = pygame.Rect(30, 30, 30*(1.0-percentage), 30*(1.0-percentage))
+        pygame.draw.rect(screen, candle.color, rectangle)
+        pygame.display.flip()
         clock.tick(60)
 
 if __name__ == "__main__":
